a7fcf6afba00e492aad67b671da0d556e7d293ce
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2024-07-12 23:26:39 +0200

    Create title list in script.js/remove title links from index.html

--- js/script.js
+++ js/script.js
@@ -42,8 +42,51 @@ function titleClickHandler(event) {
 
 }
 
-const links = document.querySelectorAll('.titles a');
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+function generateTitleLinks() {
+    console.log('Title links were generated')
+
+    /* remove contents of titleList */
+
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+
+    /* for each article */
+
+    const articles = document.querySelectorAll(optArticleSelector);
+    let html = '';
+    for (let article of articles) {
+
+    /* get the article id */
+
+    const articleId = article.getAttribute('id');
+
+    /* find the title element and get the title from the title element */
+
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+    /* create HTML of the link */
+
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+  
+    /* insert link into titleList */
+
+    //titleList.insertAdjacentHTML("beforeend", linkHTML);
+    html = html + linkHTML;
+  }
+
+  titleList.innerHTML = html;
+  console.log(html);
+
+  const links = document.querySelectorAll('.titles a');
+  console.log(links);
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
 }
+
+generateTitleLinks();
